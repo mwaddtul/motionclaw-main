7f63747081b79b912ff80a25bdf94dd06dfd17fc
mwaddtul <mawaddatulnabilarida1@gmail.com>
2024-06-18 16:55:58 +0700

    first commit

---
+++ data/projectsData.js
@@ -0,0 +1,115 @@
+const projectsData = [
+  {
+    title:
+      'Pengenalan Kehidupan Kampus Bagi Mahasiswa Baru Universitas Malikussaleh (PKKMB UNIMAL) 2019',
+    description: `The motionclaw labs team documented the PKKMB UNIMAL activities which were carried out for 3 days. in this video our team packs a grand and exciting whole activity in 5 minutes duration.`,
+    imgSrc: '/static/images/pkkmb1.gif',
+    href: 'https://www.youtube.com/watch?v=XWhL4m8Bzf0',
+  },
+  {
+    title:
+      'Mozaik Pengenalan Kehidupan Kampus Bagi Mahasiswa Baru Universitas Malikussaleh (PKKMB UNIMAL) 2019',
+    description: `During the PKKMB activity, UNIMAL also carried out mosaic activities, so the motionclaw team made a video of this magnificent activity.`,
+    imgSrc: '/static/images/pkkmb2.gif',
+    href: 'https://www.youtube.com/watch?v=18HIyLZeYNM&ab_channel=MotionclawLabs',
+  },
+  {
+    title:
+      'Tagline Pengenalan Kehidupan Kampus Bagi Mahasiswa Baru Universitas Malikussaleh (PKKMB UNIMAL) 2019',
+    description: `This video was made to promote the Tagline of PPKMB UNIMAL activities. the videos that we pack by taking a lot of footage from different people by conveying the relevant tagline.`,
+    imgSrc: '/static/images/pkkmb3.gif',
+    href: 'https://www.youtube.com/watch?v=ASqtoVJbaWQ&ab_channel=MotionclawLabs',
+  },
+  {
+    title: 'HUT Bank Rakyat Indonesia Cabang Kota Lhokseumawe 2019',
+    description: `When anniversary of Bank Rakyat Indonesia, the Motionclaw Labs Team collaborated with BRI.`,
+    imgSrc: '/static/images/bri2019.gif',
+    href: 'https://www.youtube.com/watch?v=evb5Er0TMJQ&ab_channel=MotionclawLabs',
+  },
+  {
+    title: 'Penanaman Pohon Serentak SATBRIMOB POLDA ACEH',
+    description: `This activity was carried out by the BRIMOB Aceh Utara. The Motionclaw Labs team collaborates in documenting activities that will become a media campaign in forest reforestation.`,
+    imgSrc: '/static/images/brimob.gif',
+    href: 'https://www.youtube.com/watch?v=4rc2YMTs2-c&ab_channel=MotionclawLabs',
+  },
+  {
+    title: 'Gema Safa Festival',
+    description: `Gema Safa Festival was held by Magnum. The Motionclaw Labs team becomes the documentation committee at the festival. where we make video documentation during the event.`,
+    imgSrc: '/static/images/gemasafa.gif',
+    href: 'https://www.youtube.com/watch?v=dvxVPwI10Tw&ab_channel=MotionclawLabs',
+  },
+  {
+    title: '3D HUT Lhokseumawe 18',
+    description: `3D Concept Stage of HUT Lhokseumawe 18,`,
+    imgSrc: '/static/images/hutlhokconcept.png',
+    href: 'https://drive.google.com/drive/folders/15M-sXPNIKYTFp0uBEpPzmIBzlcbAP1Kf',
+  },
+  {
+    title: 'HUT Lhokseumawe 18',
+    description: `Video Documentation of HUT Lhokseumawe 18.`,
+    imgSrc: '/static/images/hutlhok18.png',
+    href: 'https://rr3---sn-npoldn7l.c.drive.google.com/videoplayback?expire=1660994920&ei=KI0AY7zOKfGfrvIP2auo2AY&ip=36.79.6.98&cp=QVRLUkpfU1lQQVhPOmM5dUZzNG1PZklnYTBsRzdfcnBqeVR4RGtYdVVMZ1pXLWRyQ2tFa1dKbFo&id=289b56b31197068b&itag=22&source=webdrive&requiressl=yes&ttl=transient&susc=dr&driveid=1nPmZaI5S9MbTSiBFqHc9L3xCMvSTE0lP&app=explorer&mime=video/mp4&vprv=1&prv=1&dur=821.916&lmt=1660940563826105&subapp=DRIVE_WEB_FILE_VIEWER&txp=0011224&sparams=expire,ei,ip,cp,id,itag,source,requiressl,ttl,susc,driveid,app,mime,vprv,prv,dur,lmt&sig=AOq0QJ8wRAIgYJiGSWYnGqSi-EI1EnShgZwuye69VQ5Hof4GnnCicfsCICACtVEOnd0oSF55urzYtWhdw34cmotLl1STgYRE_poB&cpn=rS7pnmEMz4xJCvGt&c=WEB_EMBEDDED_PLAYER&cver=1.20220817.01.00&redirect_counter=1&cm2rm=sn-npo667s&req_id=4787a049c2eda3ee&cms_redirect=yes&cmsv=e&mh=Q5&mm=34&mn=sn-npoldn7l&ms=ltu&mt=1660980280&mv=m&mvi=3&pl=19&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRgIhAPoqTPdJI2E8ZdYPGGpGxEBzI1vlabeMqEs0bjuRCkmlAiEAw4peRiM-JJnM-PHd04yVQ-dZxQgqr7p__HYm66NutHw%3D',
+  },
+  {
+    title: 'Universitas Malikussaleh',
+    description: `Universitas Malikussaleh Profile.`,
+    imgSrc: '/static/images/unimal.png',
+    href: 'https://www.youtube.com/watch?v=hHTyKuwa3Sg&ab_channel=Bro%27SHQ',
+  },
+  {
+    title: 'Project DAK',
+    description: `Dinas Pekerjaan Umum Lhokseumawe.`,
+    imgSrc: '/static/images/dak.png',
+    href: 'https://drive.google.com/drive/folders/1ZaLKroWrq3c0KBNR0AoE-WdvQK6p6BHd',
+  },
+  {
+    title: 'Millennial Road Safety Festival 2019',
+    description: `Millennial Road Safety Festival 2019.`,
+    imgSrc: '/static/images/roadsafety.png',
+    href: 'https://drive.google.com/drive/folders/1uTc3ENU5n5drRtzRQyRLh9HGtayWCWer',
+  },
+  {
+    title: '3D Design of Kejaksaan Negeri Lhokseumawe',
+    description: `3D Design Concept of Kejaksaan Negeri Lhokseumawe.`,
+    imgSrc: '/static/images/3dkejaksaan.png',
+    href: 'https://drive.google.com/drive/folders/1GKbpovniftTkNYEzWzfA6T1lolgq0uTU',
+  },
+  {
+    title: 'Kejaksaan Negeri Lhokseumawe',
+    description: `Structure Organization of Kejaksaan Negeri Lhokseumawe.`,
+    imgSrc: '/static/images/kejaksaan.png',
+    href: 'https://drive.google.com/drive/folders/1BKH8txsUmCSUqNOg9OPxMASBWe12Z1YC',
+  },
+  {
+    title: 'International Conference on Multidisciplinary Engineering (ICoMdEn) 2018.',
+    description: `Video Documentation International Conference on Multidisciplinary Engineering (ICoMdEn) 2018.`,
+    imgSrc: '/static/images/icomden.png',
+    href: 'https://www.youtube.com/watch?v=K8GRpeqscGA&ab_channel=FakultasTeknikUniversitasMalikussaleh',
+  },
+  {
+    title: 'Inovasi Desa 2019 Aceh',
+    description: `Videos of Inovasi Desa 2019.`,
+    imgSrc: '/static/images/inovasidesa2019.png',
+    href: 'https://drive.google.com/drive/folders/1oJycZfrUDdstOehYJvMuSJF6Hg_j2pEE',
+  },
+  {
+    title: 'Inovasi Desa 2018 Aceh',
+    description: `Videos of Inovasi Desa 2018.`,
+    imgSrc: '/static/images/inovasidesa2018.png',
+    href: 'https://drive.google.com/drive/folders/1WdZdzRQRGD03zBsMwbGSsPFVlRVr2Tcy',
+  },
+  {
+    title: 'Dies Natalis Universitas Malikussaleh ke-16 2017',
+    description: `Memperingati Dies Natalis Universitas Malikussaleh ke-16 pada tahun 2017.`,
+    imgSrc: '/static/images/diesnatalis.jpg',
+    href: 'https://www.youtube.com/watch?v=26_5GzLrjZw&ab_channel=Bro%27SHQ',
+  },
+  {
+    title: 'Malam Puncak Dies Natalis Universitas Malikussaleh ke-16 2017',
+    description: `Memperingati Dies Natalis Universitas Malikussaleh ke-16 pada tahun 2017.`,
+    imgSrc: '/static/images/diesnatalis2.jpg',
+    href: 'https://www.youtube.com/watch?v=V3wUagd0TcE&ab_channel=Bro%27SHQ',
+  },
+]
+
+export default projectsData
